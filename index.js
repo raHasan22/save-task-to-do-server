e884e8f81365859866f1fcc1dbd1aaee06fea1c9
Ahmad Hasan <r.ahmad.hasan.r@gmail.com>
2022-12-29 19:25:46 +0600

    finl commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -16,6 +16,54 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const taskList = client.db('task-manager').collection('task-to-do');
+        
+        app.post('/tasks', async(req, res) =>{
+            const task = req.body
+            const result = await taskList.insertOne(task);
+            res.send(result);
+        })
+
+        app.get('/tasks/:completed', async(req, res) => {
+            const completed = req.params.completed;
+            const email = req.query.email;
+            const query = {completed: completed, email: email};
+            const result = await taskList.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/tasks/delete/:id', async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await taskList.deleteOne(filter);
+            res.send(result);
+        } )
+
+        app.put('/tasks/true/:id', async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    completed: 'true'
+                }
+            }
+            const result = await taskList.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        app.put('/tasks/false/:id', async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    completed: 'false'
+                }
+            }
+            const result = await taskList.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+        
     }
     finally{
 
